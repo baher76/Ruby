538ab27ef4222c92b2fe584a45b9265053836600
baher76 <baher76@yahoo.com>
2018-03-03 22:51:19 -0500

    Preparing for application launch

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..48
+  first_name= Faker::Name.first_name
+  last_name= Faker::Name.last_name
+  name = "#{first_name} #{last_name}"
+  email= Faker::Internet.free_email(name)
+
+  user = User.new
+  user.first_name = first_name
+  user.last_name = last_name
+  user.email = email
+
+  if user.save
+    for ii in 1..48
+      micropost = Micropost.new
+      micropost.user = user
+      micropost.message = Faker::Lorem.paragraph
+      micropost.save
+
+    end
+  end
+end
